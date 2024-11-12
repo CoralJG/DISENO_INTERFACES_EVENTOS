501143cc7875c389e26b1957267ca32890e1b442
Martín Morente Vargas <martin.dev45@gmail.com>
2024-11-12 14:34:18 +0100

    Updated Carru

---
+++ Paula_landing_wishlist/script.js
@@ -0,0 +1,17 @@
+var movement = document.getElementById("items");
+var maxScrollLeft = movement.scrollWidth - movement.clientWidth;
+var intervalo;
+
+const start = () => {
+let step = 1;
+  intervalo = setInterval(function () {
+    movement.scrollLeft = movement.scrollLeft + step;
+	if (movement.scrollLeft === maxScrollLeft) {
+      step = step * -1;
+    } else if (movement.scrollLeft === 0) {
+      step = step * -1; 
+    }
+  }, 10);
+};
+
+//start();
