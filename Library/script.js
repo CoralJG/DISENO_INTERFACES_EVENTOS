e4a0294847e780da013534f12aa5487dda57ae8f
Martín Morente Vargas <martin.dev45@gmail.com>
2024-11-15 10:30:17 +0100

    Actialización Estilos Library

--- Library/script.js
+++ Library/script.js
@@ -2,24 +2,48 @@ function mostrarBiblioteca() {
     const contenedorJuegos = document.querySelector('.list_games');
     const biblioteca = JSON.parse(localStorage.getItem('libreria')) || [];
     contenedorJuegos.innerHTML = '';
-    biblioteca.forEach((juego, index) => {
+    biblioteca.forEach((juego) => {
         const juegoHTML = `
-            <div class="games">
-                <img src="${juego.imagen}" alt="${juego.titulo}">
-                <div class="description-game">
-                    <h3>${juego.titulo}</h3>
-                    <p>${juego.description}</p>
-                    <button class="del" data-index="${index}">Delete</button>
-                </div>
+            <div class="games" style="background-image: url(${juego.imagen}); background-size:cover;">
             </div>
         `;
         contenedorJuegos.innerHTML += juegoHTML;
     });
 
-    document.querySelectorAll('.del').forEach(boton => {
-        boton.addEventListener('click', eliminarJuego);
-    });
+	let games_cont = document.querySelector('.games');
+
+	games_cont.addEventListener('click', () => {
+		const new_html = 
+		`<div class="delete_cont" style="opacity: 0; transform: translateY(20px); transition: opacity 2s ease, transform 2s ease;">
+			<div style="padding:10px;">
+				<button id="instal" style="width:100px; background-color:#02911F; border:none; height: 25px; border-radius: 5px; cursor: pointer;">Install</button>
+				<button id="del" style="width:100px; margin-left:180px; background-color: #800000; border:none; height: 25px; border-radius: 5px; cursor: pointer;">Delete</button>
+			</div>
+		</div>`
+		contenedorJuegos.innerHTML += new_html;
+	  
+		const newDiv = contenedorJuegos.querySelector('.delete_cont');
+		
+		setTimeout(() => {
+		  newDiv.style.opacity = '1';
+		  newDiv.style.transform = 'translateY(0)';
+		}, 10);
+
+		document.querySelectorAll('#del').forEach(boton => {
+			boton.addEventListener('click', eliminarJuego);
+		});
+
+		document.querySelectorAll('#instal').forEach(boton => {
+			boton.addEventListener('click', alert_install);
+		});
+	  });
 }
+
+function alert_install()
+{
+	alert('Juego instalado');
+}
+
 function eliminarJuego(event) {
     const index = event.target.dataset.index;
     let biblioteca = JSON.parse(localStorage.getItem('libreria')) || [];
